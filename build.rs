ef07ad212078d300efd41f6dc9847eafec46fd20
Geordon Worley <vadixidav@gmail.com>
2016-12-29 21:53:13 -0500

    fix build script to work as an external crate properly

--- build.rs
+++ build.rs
@@ -1,5 +1,5 @@
 use std::process::Command;
-use std::env;
+use std::path::PathBuf;
 
 #[cfg(target_os="macos")]
 fn print_lib_dir() {
@@ -7,13 +7,17 @@ fn print_lib_dir() {
 }
 
 #[cfg(not(target_os="macos"))]
-fn print_lib_dir() {
-}
+fn print_lib_dir() {}
 
 fn main() {
-    let curr_dir = env::current_dir().unwrap();
-    Command::new("git").arg("submodule").arg("update").arg("--init")
-        .current_dir(&curr_dir).status().unwrap();
+    let curr_dir = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
+    Command::new("git")
+        .arg("submodule")
+        .arg("update")
+        .arg("--init")
+        .current_dir(&curr_dir)
+        .status()
+        .unwrap();
 
     let nvim_dir = curr_dir.join("neovim");
     Command::new("make").arg("deps").current_dir(&nvim_dir).status().unwrap();
@@ -22,8 +26,8 @@ fn main() {
     let nvim_lib_dir = nvim_dir.join("build").join("lib");
     let deps_lib_dir = nvim_dir.join(".deps").join("usr").join("lib");
     println!("cargo:rustc-flags=-L {} -L {} -l nvim",
-        nvim_lib_dir.to_str().unwrap(),
-        deps_lib_dir.to_str().unwrap());
+             nvim_lib_dir.to_str().unwrap(),
+             deps_lib_dir.to_str().unwrap());
 
     print_lib_dir();
 }
